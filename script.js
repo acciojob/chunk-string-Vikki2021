bd7c2191a6ed2e22a873898dd5d9c07838aaad6f
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-12 15:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
 function stringChop(str, size) {
   // your code here
 	if (!str) return []; 
 		
 	let result = [];
 	for (let i = 0; i < str.length; i += chunkLength) {
-	        result.push(str.substring(i, i + chunkLength));
+	    result.push(str.substring(i, i + chunkLength));
 	}
     
 	return result;
